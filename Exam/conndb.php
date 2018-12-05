34f22308ddec5ff7b58c7abec56ed9e0cc142e9e
Dubois Matthieu <dubois.matthieu@live.be>
2018-12-05 13:30:26 +0100

    Remove conndb from distant repo

--- Exam/conndb.php
+++
@@ -1,7 +0,0 @@
-<?php
-  try {
-    $pdo=new PDO('mysql:host=localhost; dbname=courses2018; charset=utf8', 'root', '');
-  } catch (PDOException $e) {
-    echo 'Erreur de connexion: ' . $e->getMessage();
-  }
-?>
