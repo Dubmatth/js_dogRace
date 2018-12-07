132af902a7eb5f885d7fec84fd31171ba01979eb
Dubois Matthieu <dubois.matthieu@live.be>
2018-12-07 14:31:19 +0100

    Finalisation du select des animaux

--- Exam/race.js
+++ Exam/race.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const infosRace = {}
     const mainDiv = document.getElementById('mainDiv')
     const choiceAnimals = document.getElementById('choiceAnimals')
+    const participants = document.getElementById('participants')
 
 
     // ------ We fill the countries ------ //
@@ -21,6 +22,23 @@ document.addEventListener('DOMContentLoaded', function(){
                     let option = document.createElement('option')
                     option.append(data.nomP)
                     countryRace.append(option)
+                }
+            })
+    }
+
+    // ------ Fill the animals ------ //
+    let allDatasAnimals = []
+    function selectAnimals(){
+        fetch('./rqListeAnimaux.php')
+            .then(res => res.json())
+            .then(datas => {
+                for(data of datas){
+                    allDatasAnimals = datas
+                    let option = document.createElement('option')
+                    option.append(data.nomA)
+                    option.value = data.idA
+                    participants.append(option)
+
                 }
             })
     }
@@ -52,9 +70,55 @@ document.addEventListener('DOMContentLoaded', function(){
         infosRace.duration = '2000ms'
         infosRace.pays = countryRace.value
 
-        // --- Call choiceAnimals --- //
+        // --- Fill & Call choiceAnimals --- //
+        selectAnimals()
         durationRace.after = choiceAnimals
         choiceAnimals.hidden = false
+    }
+
+    // ------ Add animals to inscription ------ //
+    const rowPlayer = document.getElementById('rowPlayer')
+    let allPlayers = []
+    participants.onchange = addToInscription
+    function addToInscription(){
+
+        const tbodyPlayer = document.getElementById('tbodyPlayer')
+        const currentPlayer = allDatasAnimals.find(currentPlayer => currentPlayer.idA === participants.value)
+        allPlayers.push(currentPlayer)
+        const toDisabled = participants.selectedIndex
+        participants.options[toDisabled].setAttribute('disabled', 'disabled')
+
+        // --- Create tr & his elements --- //
+        const clonedRowPlayer = rowPlayer.cloneNode()
+
+        const tdBtn = document.createElement('td')
+        const tdBtnSupp = document.createElement('button')
+              tdBtnSupp.textContent = 'Supprimer'
+              tdBtnSupp.classList = 'btn btn-primary'
+        const tdIdA = document.createElement('td')
+        const tdNomA = document.createElement('td')
+        const tdNatA = document.createElement('td')
+        const tdDescA = document.createElement('td')
+
+        tdBtn.append(tdBtnSupp)
+        tdIdA.append(currentPlayer.idA)
+        tdNomA.append(currentPlayer.nomA)
+        tdNatA.append(currentPlayer.nationA)
+        tdDescA.append(currentPlayer.descA)
+
+        clonedRowPlayer.append(tdBtn)
+        clonedRowPlayer.append(tdIdA)
+        clonedRowPlayer.append(tdNomA)
+        clonedRowPlayer.append(tdNatA)
+        clonedRowPlayer.append(tdDescA)
+
+        // --- Add the row at the table tbody --- //
+        tbodyPlayer.append(clonedRowPlayer)
+
+        // --- Change the default selected element --- //
+        participants.options[0].setAttribute('selected', 'selected')
+        participants.options[0].removeAttribute('selected', 'selected')
+
     }
 
 
